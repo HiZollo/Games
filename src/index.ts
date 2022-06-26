6c6684a02b0b715b303296725880ee54229fec50
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2022-06-26 12:05:02 +0800

    export interfaces

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,56 @@ export { DjsFlipTrip } from './djsgames/DjsFlipTrip';
 export { DjsGomoku } from './djsgames/DjsGomoku';
 export { DjsLightsUp } from './djsgames/DjsLightsUp';
 export { DjsTicTacToe } from './djsgames/DjsTicTacToe';
+
+export { GameUtil } from './util/GameUtil';
+
+export { 
+  PlayerOptions, 
+  PlayerManagerOptions, 
+  GameOptions, 
+
+  BullsAndCowsOptions, 
+  FinalCodeOptions, 
+  FlipTripOptions, 
+  GomokuOptions, 
+  LightsUpOptions, 
+  TicTacToeOptions, 
+
+  DjsGameWrapperOptions, 
+  DjsBullsAndCowsOptions, 
+  DjsFinalCodeOptions, 
+  DjsFlipTripOptions, 
+  DjsGomokuOptions, 
+  DjsLightsUpOptions, 
+  DjsTicTacToeOptions, 
+
+  IBullsAndCows, 
+  IFinalCode, 
+  IFlipTrip, 
+  IGomoku, 
+  ILightsUp, 
+  ITicTacToe, 
+
+  BullsAndCowsResult, 
+
+  GameStrings, 
+  BullsAndCowsStrings, 
+  FinalCodeStrings, 
+  FlipTripStrings, 
+  GomokuStrings, 
+  LightsUpStrings, 
+  TicTacToeStrings, 
+
+  ControllerStrings, 
+  LightsUpControllerStrings, 
+
+  EndMessageStrings, 
+  FlipTripEndMessageStrings, 
+  GomokuEndMessageStrings, 
+  LightsUpEndMessageStrings, 
+  TicTacToeEndMessageStrings, 
+
+  FinalCodePreviousStrings, 
+  GomokuPreviousStrings, 
+  TicTacToePreviousStrings
+} from './types/interfaces'
